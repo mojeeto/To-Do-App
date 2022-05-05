81980d44e73aa1fca965b5225e93fe5a93a3cad2
Mojeeto <mojtaba01nouri@gmail.com>
2022-05-05 11:22:04 +0430

    make AboutViewController for handle buttons and navigate to it of AboutScreen

---
+++ To-Do App/Controllers/AboutViewController.swift
@@ -0,0 +1,24 @@
+//
+//  AboutViewController.swift
+//  To-Do App
+//
+//  Created by Mojtaba Nouri on 05/05/2022.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBOutlet weak var mojeetoImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func contactButton(_ sender: UIButton) {
+    }
+    
+
+}
--- To-Do App/Controllers/ToDoTableViewController.swift
+++ To-Do App/Controllers/ToDoTableViewController.swift
@@ -6,14 +6,44 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoTableViewController: UITableViewController {
+    
+    var allData = [Categories(): [Items]()]
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
+    //MARK: - manipulation coredata
+    func saveData() {
+//        save data on core data
+        do { try context.save() } catch { print("Error while saveing data, \(error)") }
+    }
+    
+    func loadData(withCategory category: Categories, predicate: NSPredicate = NSPredicate(value: true)) {
+//        create request
+        let request: NSFetchRequest<Items> = Items.fetchRequest()
+//        compact all predicate
+        request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [NSPredicate(format: "category.name MATCHES %@", category.name!), predicate])
+//        try fetch data from core data
+        do { allData[category] = try context.fetch(request) } catch { print("Error while fetchData, \(error)") }
+    }
+    
+    
+    //MARK: - navigation buttons action
+    @IBAction func aboutButton(_ sender: UIBarButtonItem) {
+//        go to about screen
+        performSegue(withIdentifier: "goToAbout", sender: self)
+    }
+    
+    @IBAction func addToDoButton(_ sender: UIBarButtonItem) {
+    }
+    
 }
 
