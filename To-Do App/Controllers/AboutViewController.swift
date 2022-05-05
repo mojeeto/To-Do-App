540afc9fee11a5c27572c1e74f463e4b52d7af45
Mojeeto <mojtaba01nouri@gmail.com>
2022-05-05 21:01:45 +0430

    add some radius and contact way to me at AboutScreen

--- To-Do App/Controllers/AboutViewController.swift
+++ To-Do App/Controllers/AboutViewController.swift
@@ -13,11 +13,25 @@ class AboutViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        mojeetoImageView.layer.cornerRadius = 75
         // Do any additional setup after loading the view.
     }
     
     @IBAction func contactButton(_ sender: UIButton) {
+        if let safeTitleLable = sender.titleLabel?.text {
+            if safeTitleLable == "E-Mail" {
+                guard let url = URL(string: "mailto:mojtaba01nouri@gmail.com") else { return }
+                UIApplication.shared.open(url)
+            }
+            else if safeTitleLable == "Github" {
+                guard let url = URL(string: "https://github.com/mojeeto") else { return }
+                UIApplication.shared.open(url)
+            }
+            else {
+                guard let url = URL(string: "https://instagram.com/_mojeeto") else { return }
+                UIApplication.shared.open(url)
+            }
+        }
     }
     
 
